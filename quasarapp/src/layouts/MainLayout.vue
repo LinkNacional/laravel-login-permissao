1902236232941d65301b0ced98c713bc8d7088f4
JoaoVictorLNacional <viana.joao@linknacional.com>
2021-03-18 17:11:25 -0300

    add tabela dos usuarios no quasar

--- quasarapp/src/layouts/MainLayout.vue
+++ quasarapp/src/layouts/MainLayout.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>teste</div>
+<q-layout>
+  <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
 export default {
   name: 'MainLayout',
   components: {},
-  data() {
+  data () {
     return {}
   }
 }
---
+++ quasarapp/src/pages/tableUser.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <tableList title="Usuarios"/>
+  </q-page>
+</template>
+
+<script>
+import tableList from 'src/components/tableList.vue'
+export default {
+  components: { tableList },
+  name: 'tableUser'
+}
+</script>
